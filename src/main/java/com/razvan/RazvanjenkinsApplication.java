5be706c1c5c93680e3f233fea3018a6d50e53605
RazvanRC <3emporniumnet1>
2017-04-03 16:22:03 +0200

    initial commit

---
+++ src/main/java/com/razvan/RazvanjenkinsApplication.java
@@ -0,0 +1,12 @@
+package com.razvan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RazvanjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RazvanjenkinsApplication.class, args);
+	}
+}
